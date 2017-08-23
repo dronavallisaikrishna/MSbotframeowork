750f514abfff0fad0b006557743e331af0a59a04
saikrishna <dronavalli.saikrishna@gmail.com>
2017-08-23 15:52:18 +0530

    commit after new meesage

--- app.js
+++ app.js
@@ -46,5 +46,5 @@ server.get(/.*/, restify.serveStatic({
 }));
 
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
